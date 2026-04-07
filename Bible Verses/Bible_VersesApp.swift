09da50f04fafc70dfbe36ccedc80cc07624d85c0
Gabby <gabriellelaah@gmail.com>
2026-04-07 10:38:31 -0500

    Initial Commit

---
+++ Bible Verses/Bible_VersesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_VersesApp.swift
+//  Bible Verses
+//
+//  Created by gabrielle on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Bible_VersesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bible Verses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bible Verses
+//
+//  Created by gabrielle on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
